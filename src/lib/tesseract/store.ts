38a21926da2eee3a554f8b99bd8808fc6cd2326a
Hannes vonSarahsLaptop <HannesGitH@users.noreply.github.com>
2022-01-15 16:46:54 +0100

    so stell ich mir ungefähr den recognizer vor

---
+++ src/lib/tesseract/store.ts
@@ -0,0 +1,27 @@
+import Tesseract, { createWorker } from 'tesseract.js';
+import Store, { readable, derived, writable } from 'svelte/store';
+
+const worker = createWorker({
+	logger: (m) => console.log(m)
+});
+
+export const image : Store.Writable<Tesseract.ImageLike> = writable();
+
+const _processor = derived(
+    image,
+    $image => _recognize($image).then(res => _result.set(res))
+);
+
+const _result : Store.Writable<Tesseract.RecognizeResult> = writable();
+
+const _recognize = async (image: Tesseract.ImageLike) : Promise<Tesseract.RecognizeResult> => {
+	await worker.load();
+	await worker.loadLanguage('eng');
+	await worker.initialize('eng');
+	const result = await worker.recognize(image);
+	console.log(result.data.text);
+	await worker.terminate();
+    return result;
+};
+
+export const result : Store.Readable<Tesseract.RecognizeResult> = {subscribe: _result.subscribe};
