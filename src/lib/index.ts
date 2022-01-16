c1e2d4d26c8fee6e6034c3629e9332ba07cbd658
Hannes vonSarahsLaptop <HannesGitH@users.noreply.github.com>
2022-01-16 23:12:33 +0100

    okay it runs (npm run build && npm run preview)

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,16 +1,16 @@
-//import Tesseract, { createWorker } from 'tesseract.js';
+import Tesseract, { createWorker } from 'tesseract.js';
 import { readable, derived, writable, 
 Writable, Readable} from 'svelte/store';
 
 /// create js-worker to run tesseract, also make him update the _progress state
-// const worker = createWorker({
-// 	logger: (m) => {
-// 		_progress.update((p) => <Progress>{ status: m.status, progress: m.progress });
-// 	}
-// });
+const worker = createWorker({
+	logger: (m) => {
+		_progress.update((p) => <Progress>{ status: m.status, progress: m.progress });
+	}
+});
 
 /// set this writeable to the image that shall be scanned
-export const image/*: Writable<Tesseract.ImageLike>*/ = writable();
+export const image: Writable<Tesseract.ImageLike> = writable();
 
 
 interface Progress {
@@ -21,25 +21,26 @@ interface Progress {
 }
 
 /// initializes the worker if not already initialized
-export const initialize = async ()=>{
-	if(_is_initialized)return;
-	// await worker.load();
-	// await worker.loadLanguage('eng');
-	// await worker.initialize('eng');
+export const initialize = async ():Promise<Boolean>=>{
+	if(_is_initialized)return true;
+	await worker.load();
+	await worker.loadLanguage('eng');
+	await worker.initialize('eng');
 	_is_initialized = true;
+	return false;
 }
 
 /// cleans up all recognition stuff if its no longer needed
 export const terminate = async ()=>{
 	_is_initialized=false;
-	//await worker.terminate();
+	await worker.terminate();
 }
 
 /// stores whether the worker is ready for recognition
 let _is_initialized = false;
 
 /// combines status and progress into one store
-const _progress: Writable<Progress> = writable();
+const _progress = writable(<Progress>{status:'not-started', progress:0});
 
 
 /// what step of the recognition are we currently in
@@ -49,22 +50,26 @@ export const status = derived(_progress, ($_progress) => $_progress.status);
 export const progress = derived(_progress, ($_progress) => $_progress.progress);
 
 
-const _recognize = async (image/*: Tesseract.ImageLike*/)/*: Promise<Tesseract.RecognizeResult>*/ => {
+const _recognize = async (image: Tesseract.ImageLike): Promise<Tesseract.RecognizeResult> => {
 	//make sure worker is ready
 	await initialize();
-	// const result = await worker.recognize(image);
-	// console.log({text_found: result.data.text});
-	// return result;
+	const result = await worker.recognize(image);
+	console.log({text_found: result.data.text});
+	return result;
 };
 
 /// the fully writeable result store, but we dont want to expose the setters..
-const _result/*: Writable<Tesseract.RecognizeResult>*/ = writable();
+const _result: Writable<Tesseract.RecognizeResult> = writable();
 
 /// readable result directly from tesseract with all its data
-export const result/*: Readable<Tesseract.RecognizeResult>*/ = { subscribe: _result.subscribe };
+export const result: Readable<Tesseract.RecognizeResult> = { subscribe: _result.subscribe };
 
 /// all the resulting text
-export const allText = derived(result, ($result) => /*$result.data.text*/ "breb");
+export const allText = derived(result, ($result) => {try {
+	return $result.data.text
+} catch (error) {
+	return 'error' + error
+} });
 
 
 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,10 @@
 <script>
-	import {image} from '$lib/index'
+	import {initialize, allText, image} from '$lib/index'
+	(async()=>{
+		console.log(await initialize());
+		image.set('https://tesseract.projectnaptha.com/img/eng_bw.png');
+	})()
 </script>
 
 <h1>Welcome</h1>
+<p>{$allText}</p>
